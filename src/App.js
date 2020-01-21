eb1da28d9205aab458c39200b694842ab395857a
apple <apple@apples-MacBook-Pro-2.local>
2020-01-21 15:46:06 -0800

    periodic save

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
+import PostsPage from "./components/PostsContainer/PostsPage";
+import SearchBar from './components/SearchBar/SearchBarContainer';
 // import dummydata
+import dummydata from '../src/dummy-data';
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
